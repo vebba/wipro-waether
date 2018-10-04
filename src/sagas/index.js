ba7a4b8a9832a7d392bd067d4487f1da7a76709e
niceone <pawel@niceone.io>
2018-10-04 14:57:46 +0200

    code formating

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,60 +1,56 @@
-import {call, put, takeLatest} from 'redux-saga/effects'
-import moment from 'moment'
+import { call, put, takeLatest } from "redux-saga/effects";
+import moment from "moment";
 
-
-export const api = (url) => fetch(url).then(response => response.json());
-
+export const api = url => fetch(url).then(response => response.json());
 
 const KEY = process.env.REACT_APP_WAETHER_API_KEY;
 const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,GB&appid=${KEY}&units=metric`;
 
-
-const getDayForDate = date => moment(date).format('dddd');
-
+const getDayForDate = date => moment(date).format("dddd");
 
 const normalizeState = data => {
-    const {list} = data;
-    const result = {};
-    
-    const measurements = list.reduce((acc,current) => {
-        return {...acc, [current.dt]: {...current, name: getDayForDate(current.dt_txt) }}
-    },{})
+  const { list } = data;
+  const result = {};
 
-    const daysById = list.reduce((day,curr)=>{
-            const dayname = getDayForDate(curr.dt_txt);
-            if(!day[dayname]) {
-                day[dayname] = []
-            }
-            day[dayname].push(curr.dt)
-            return day 
-        },{})
-    
-    result.readings = {
-        byId : measurements,
-        allIds: Object.keys(measurements)
+  const measurements = list.reduce((acc, current) => {
+    return {
+      ...acc,
+      [current.dt]: { ...current, name: getDayForDate(current.dt_txt) }
+    };
+  }, {});
+
+  const daysById = list.reduce((day, curr) => {
+    const dayname = getDayForDate(curr.dt_txt);
+    if (!day[dayname]) {
+      day[dayname] = [];
     }
-    
-    result.days = {
-        byId: daysById,
-        allIds: Object.keys(daysById)
-    }
+    day[dayname].push(curr.dt);
+    return day;
+  }, {});
 
+  result.readings = {
+    byId: measurements,
+    allIds: Object.keys(measurements)
+  };
 
-    return result
-}
+  result.days = {
+    byId: daysById,
+    allIds: Object.keys(daysById)
+  };
+
+  return result;
+};
 
 function* fetchApiData() {
-    try {
-
-         const response = yield call(api, API_URL);
-        const data = response;
-        yield put({type: 'DATA_SUCCESS', payload: normalizeState(data) });
-
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const response = yield call(api, API_URL);
+    const data = response;
+    yield put({ type: "DATA_SUCCESS", payload: normalizeState(data) });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default function* mainSaga() {
-    yield takeLatest('REQUEST_API_DATA', fetchApiData);
+  yield takeLatest("REQUEST_API_DATA", fetchApiData);
 }
