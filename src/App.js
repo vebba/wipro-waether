781e3e1e4dc9c4ca8bf133ed8804d3da2ac6229b
niceone <pawel@niceone.io>
2018-10-04 16:13:52 +0200

    header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import WeatherList from './components/WeatherList';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+          <Header/>
           <WeatherList />
-        </header>
+
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Weather in London</h1>
+    </div>
+  )
+}
+
+export default Header
+
